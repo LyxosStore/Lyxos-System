c0a4b8c1e8f0dd944b402dc74526881c9ab322b6
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2023-12-21 05:01:07 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    prefix: "!",
+    owner: "813300902836043797",
+    token: "",
+    autoRoleNameOnJoin: "",
+    welcomeChannelId: "",
+    goodbyeChannelId: "",
+    rulesChannelId: "",
+    logChannelId: "",
+    mysqlHost: "",
+    mysqlUser: "",
+    mysqlPassword: "",
+    mysqlDatabase: ""
+}
